249df63436f0564e04963b718b5a70fd0d53e00e
Shirui Ma <mashirui521@gmail.com>
2015-10-07 22:03:38 +0200

    add unit tests

---
+++ utest/UnitTest.java
@@ -0,0 +1,15 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import suites.UT_SUITE_01;
+
+/**
+ * @author Shirui
+ * This class is only a unit test container
+ */
+@RunWith(Suite.class)
+@SuiteClasses({UT_SUITE_01.class})
+public class UnitTest {
+
+}
---
+++ utest/suites/UT_SUITE_01.java
@@ -0,0 +1,12 @@
+package suites;
+
+import org.junit.Test;
+
+public class UT_SUITE_01 {
+
+	@Test
+	public void ut_01() {
+		
+	}
+	
+}
